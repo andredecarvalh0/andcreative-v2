c488cb9dfc976af1abe9d6bd3cfc5e2dc4a54ad4
andre <andre.decarvalho1@gmail.com>
2022-09-02 18:01:00 -0300

    update dark bg

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,3 +190,14 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+//Stars
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const STAR_COUNT = 100
+let result = ""
+for(let i = 0; i < STAR_COUNT; i++){
+    result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 3)}px ${randomNumber(0, 3)}px #fff,`
+}
+console.log(result.substring(0, result.length - 1))
