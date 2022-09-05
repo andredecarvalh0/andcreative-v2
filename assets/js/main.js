26af366d28b5c3bb9b64677da14a10ce61710060
andre <andre.decarvalho1@gmail.com>
2022-09-05 17:25:29 -0300

    v1.1 update

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,11 +137,11 @@ function scrollActive(){
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        // if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-        //     document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
-        // }else{
-        //     document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        // }
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
     })
 }
 window.addEventListener('scroll', scrollActive)
